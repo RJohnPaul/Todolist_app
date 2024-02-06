39661ae1675f896de3caeeb03f64380bf7ec5ebc
John Paul <iamjohnpaulr5@gmail.com>
2024-02-06 21:21:00 +0530

    Add react-scroll package and its dependencies

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+import {Linkedin} from "@styled-icons/boxicons-logos/Linkedin";
+import {Github} from "@styled-icons/boxicons-logos/Github";
 import { ReactLogo } from "styled-icons/boxicons-logos";
+import { TailwindCss } from "styled-icons/boxicons-logos";
 
 export default function Home() {
+  const [showVideo, setShowVideo] = useState(false);
+
+  const handlePlayClick = () => {
+    setShowVideo(true);
+  };
   return (
     <>
       <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
@@ -65,59 +75,63 @@ export default function Home() {
             className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
           >
             <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:items-center md:justify-end md:gap-y-0 md:gap-x-7 md:mt-0 md:ps-7">
-              <a
-                className="font-medium text-blue-600 md:py-6 dark:text-blue-500"
-                href="#"
-                aria-current="page"
+              <Link
+                to="Landing"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer font-medium text-blue-600 md:py-6 dark:text-blue-500"
+                spy={true}
+                offset={-70}
               >
                 Landing
-              </a>
-              <a
-                className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
+              </Link>
+              <Link
+                to="Demo"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                spy={true}
+                offset={-70}
               >
-                Account
-              </a>
-              <a
-                className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
+                Demo
+              </Link>
+              <Link
+                to="Features"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                spy={true}
+                offset={-70}
               >
-                Work
-              </a>
-              <a
-                className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
+                Features
+              </Link>
+              <Link
+                to="FAQ"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                spy={true}
+                offset={-70}
               >
-                Blog
-              </a>
-              <a
-                className="flex items-center gap-x-2 pt-2 pb-2 font-medium text-gray-500 hover:text-blue-600 md:border-s md:border-gray-300 md:my-6 md:ps-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500"
-                href="#"
+                Faq
+              </Link>
+              <Link
+                to="Contact"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer flex items-center gap-x-2 pt-2 pb-2 font-medium text-gray-500 hover:text-blue-600 md:border-s md:border-gray-300 md:my-6 md:ps-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500"
+                spy={true}
+                offset={-70}
               >
-                <svg
-                  class="flex-shrink-0 w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
-                </svg>
-                Download
-              </a>
+                Contact
+              </Link>
             </div>
           </div>
         </nav>
       </header>
 
       {/* Hero */}
-      <div className="relative overflow-hidden">
+      <section id="Landing" className="relative overflow-hidden">
         {/* Gradients */}
 
         {/* End Gradients */}
@@ -143,10 +157,13 @@ export default function Home() {
                 </p>
               </div>
               {/* Buttons */}
-              <div className="mt-8 gap-3 flex justify-center">
+              <div
+                className="mt-8 gap-3 flex justify-center"
+                data-hs-overlay="#hs-cookies"
+              >
                 <a
                   className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold transition-all rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  href="#"
+                  href="/todolist.apk"
                 >
                   Download Apk
                   <svg
@@ -177,10 +194,10 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
       {/* End Hero */}
 
-      <div className="pt-10 relative overflow-hidden">
+      <section id="Demo" className="pt-10 relative overflow-hidden">
         <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
           <div className="max-w-2xl text-center mx-auto">
             <h1 className="block text-3xl leading-8 font-bold text-gray-800 sm:text-4xl md:text-5xl dark:text-white">
@@ -197,26 +214,37 @@ export default function Home() {
 
             <div className="absolute inset-0 w-full h-full">
               <div className="flex flex-col justify-center items-center w-full h-full">
-                <a
-                  className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  href="#"
-                >
-                  <svg
-                    class="flex-shrink-0 w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                {!showVideo ? (
+                  <button
+                    className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    onClick={handlePlayClick}
                   >
-                    <polygon points="5 3 19 12 5 21 5 3" />
-                  </svg>
-                  Play the overview
-                </a>
+                    <svg
+                      className="flex-shrink-0 w-4 h-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polygon points="5 3 19 12 5 21 5 3" />
+                    </svg>
+                    Play the overview
+                  </button>
+                ) : (
+                  <video
+                    controls
+                    className="w-150 h-150 sm:w-96 h-96 rounded-xl shadow-lg dark:shadow-gray-900"
+                    src="/todolistdemo.mp4"
+                    autoPlay
+                  >
+                    Your browser does not support the video tag.
+                  </video>
+                )}
               </div>
             </div>
 
@@ -229,8 +257,11 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      </section>
+      <section
+        id="Features"
+        className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto"
+      >
         <div className="relative p-6 md:p-16">
           <div className="relative z-10 lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
             <div className="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-8 lg:order-2">
@@ -368,7 +399,7 @@ export default function Home() {
                   >
                     <img
                       class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                      src="https://images.unsplash.com/photo-1605629921711-2f6b00c6bbf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
+                      src="https://images.unsplash.com/photo-1558655146-d09347e92766?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                       alt="Image Description"
                     />
                   </div>
@@ -381,7 +412,7 @@ export default function Home() {
                   >
                     <img
                       class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                      src="https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
+                      src="https://images.unsplash.com/photo-1583732964634-2576820523d0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3BlZWRvbWV0ZXJ8ZW58MHwxfDB8fHww"
                       alt="Image Description"
                     />
                   </div>
@@ -394,7 +425,7 @@ export default function Home() {
                   >
                     <img
                       class="shadow-xl shadow-gray-200 rounded-xl dark:shadow-gray-900/[.2]"
-                      src="https://images.unsplash.com/photo-1598929213452-52d72f63e307?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&h=1220&q=80"
+                      src="https://images.unsplash.com/photo-1618788372246-79faff0c3742?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                       alt="Image Description"
                     />
                   </div>
@@ -437,7 +468,7 @@ export default function Home() {
             <div className="col-span-full lg:col-span-7 lg:col-start-6 bg-gray-100 w-full h-5/6 rounded-xl sm:h-3/4 lg:h-full dark:bg-white/[.075]"></div>
           </div>
         </div>
-      </div>
+      </section>
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         {/* Grid */}
         <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:gap-20 md:grid-cols-3 lg:grid-cols-5 lg:gap-8">
@@ -1035,7 +1066,10 @@ export default function Home() {
         </div>
         {/* End Grid */}
       </div>
-      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <section
+        id="FAQ"
+        className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto"
+      >
         {/* Title */}
         <div className="max-w-2xl mx-auto mb-10 lg:mb-14">
           <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
@@ -1067,8 +1101,8 @@ export default function Home() {
                   How do I add tasks to my to-do list?
                 </h3>
                 <p className="mt-1 text-gray-500">
-                  To add tasks to your to-do list, simply tap the 'Add Task' button on
-                  the app's main screen and enter the task details.
+                  To add tasks to your to-do list, simply tap the 'Add Task'
+                  button on the app's main screen and enter the task details.
                 </p>
               </div>
             </div>
@@ -1220,8 +1254,11 @@ export default function Home() {
           </div>
           {/* Add more FAQs as needed */}
         </div>
-      </div>
-      <footer className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+      </section>
+      <section
+        id="Contact"
+        className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto"
+      >
         {/* Grid */}
         <div className="text-center">
           <div>
@@ -1253,69 +1290,37 @@ export default function Home() {
           <div className="mt-3 space-x-2">
             <a
               className="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800"
-              href="#"
+              href="https://tailwindcss.com/"
+              target="_blank"
             >
-              <svg
-                className="flex-shrink-0 w-3.5 h-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width={16}
-                height={16}
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-              </svg>
+              <TailwindCss className="flex-shrink-0 w-5 h-5" />
             </a>
             <a
               className="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800"
-              href="#"
+              href="https://react.dev/"
+              target="_blank"
             >
-              <svg
-                className="flex-shrink-0 w-3.5 h-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width={16}
-                height={16}
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-              </svg>
+              <ReactLogo className="flex-shrink-0 w-5 h-5" />
             </a>
             <a
               className="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800"
-              href="#"
+              href="https://github.com/RJohnPaul/Todolist_app"
+              target="_blank"
             >
-              <svg
-                className="flex-shrink-0 w-3.5 h-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width={16}
-                height={16}
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-              </svg>
+              <Github className="flex-shrink-0 w-5 h-5" />
             </a>
             <a
               className="inline-flex justify-center items-center w-10 h-10 text-center text-gray-500 hover:bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition dark:text-gray-500 dark:hover:text-gray-200 dark:hover:bg-gray-800"
-              href="#"
+              href="https://www.linkedin.com/in/john-paul-572496278/"
+              target="_blank"
             >
-              <svg
-                className="flex-shrink-0 w-3.5 h-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width={16}
-                height={16}
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M3.362 10.11c0 .926-.756 1.681-1.681 1.681S0 11.036 0 10.111C0 9.186.756 8.43 1.68 8.43h1.682v1.68zm.846 0c0-.924.756-1.68 1.681-1.68s1.681.756 1.681 1.68v4.21c0 .924-.756 1.68-1.68 1.68a1.685 1.685 0 0 1-1.682-1.68v-4.21zM5.89 3.362c-.926 0-1.682-.756-1.682-1.681S4.964 0 5.89 0s1.68.756 1.68 1.68v1.682H5.89zm0 .846c.924 0 1.68.756 1.68 1.681S6.814 7.57 5.89 7.57H1.68C.757 7.57 0 6.814 0 5.89c0-.926.756-1.682 1.68-1.682h4.21zm6.749 1.682c0-.926.755-1.682 1.68-1.682.925 0 1.681.756 1.681 1.681s-.756 1.681-1.68 1.681h-1.681V5.89zm-.848 0c0 .924-.755 1.68-1.68 1.68A1.685 1.685 0 0 1 8.43 5.89V1.68C8.43.757 9.186 0 10.11 0c.926 0 1.681.756 1.681 1.68v4.21zm-1.681 6.748c.926 0 1.682.756 1.682 1.681S11.036 16 10.11 16s-1.681-.756-1.681-1.68v-1.682h1.68zm0-.847c-.924 0-1.68-.755-1.68-1.68 0-.925.756-1.681 1.68-1.681h4.21c.924 0 1.68.756 1.68 1.68 0 .926-.756 1.681-1.68 1.681h-4.21z" />
-              </svg>
+              <Linkedin className="flex-shrink-0 w-5 h-5" />
             </a>
           </div>
           {/* End Social Brands */}
         </div>
         {/* End Grid */}
-      </footer>
+      </section>
     </>
   );
 }
